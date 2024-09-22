77b45352bc2294f37aa4c4e8ec0d679fcce9bd60
hour <thonglyhour02@gmail.com>
2024-09-22 22:50:50 +0700

    contact and resume page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,31 @@
+'use client'
+
 import React from 'react'
+import { FaPhoneAlt, FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa'
+
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: 'Phone',
+    description: '(+855) 17 782 221',
+  },
+  {
+    icon: <FaEnvelope />,
+    title: 'Email',
+    description: 'thonglyhour02@gmail.com',
+  },
+  {
+    icon: <FaGithub />,
+    title: 'Github',
+    description: 'thong-lyhour',
+  },
+  {
+    icon: <FaLinkedin />,
+    title: 'LinkedIn',
+    description: 'Lyhour Thong',
+  },
+]
 
 const Contact = () => {
   return (
